9563eee00698005b748017f478755d1ad26f5737
davidjk522 <davidjk522@gmail.com>
2025-11-21 16:27:40 +0900

    graphhopper 배포: -my2start, start2end

--- app_service/app.py
+++ app_service/app.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-from app_service.ors.routefinder import get_directions, my2start
-
 # Add the project root directory to the Python path to resolve import errors
 project_root = os.path.abspath(os.path.dirname(__file__))
 if project_root not in sys.path:
@@ -10,19 +8,14 @@ if project_root not in sys.path:
 
 from flask import Flask, request, jsonify
 import logging
-from dotenv import load_dotenv
 
 # Local application imports
 from utils import haversine, round_coord
 from findIsochrone import get_distances_batch
 from course_generator.detour import generate_detour_course
 from course_generator.loop import generate_loop_course
-import ors.routefinder
+from app_service.ors import routefinder
 
-
-# --- Setup ---
-#load_dotenv()
-#GRAPHHOPPER_URL = os.getenv("GRAPHHOPPER_URL", "http://localhost:8989/maps/")
 app = Flask(__name__) #Dockerfile에 명시 해야 함
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -160,7 +153,7 @@ def get_directions_endpoint():
     course_arr = data['course']   # multiple course list
 
     # ORS Directions Request ors/routefinder.py의 get_directions_array 함수 사용
-    directions = get_directions(origin_lat, origin_lng, course_arr)
+    directions = routefinder.get_directions(origin_lat, origin_lng, course_arr)
 
     if not directions:
         return jsonify({'success': False, 'error': 'Failed to retrieve directions from Graphhopper.'}), 500
@@ -185,10 +178,10 @@ def find_ways():
         logging.info(f"Course received: {course}")
     # ORS Directions Request ors/routefinder.py의 my2start 함수 사용
         #mys2start_route는 최상단 요소인 mypoint를 활용하므로 반드시 data 전체를 넘겨줘야 함
-        my2start_route = my2start(my_lat, my_lng,course)
+        my2start_route = routefinder.my2start(my_lat, my_lng,course)
         logging.info(f"my2start route: {my2start_route}")
 
-        start2end_route = ors.routefinder.start2end( course)
+        start2end_route = routefinder.start2end( course)
         logging.info(f"start2end route: {start2end_route}")
 
         directions = {'my2start': my2start_route, 'start2end': start2end_route}
